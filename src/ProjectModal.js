85fa169717527b9b52d018cfa421d0a389b016e9
KaushalNaresh <kaushalnaresh2689@gmail.com>
2023-12-23 23:51:38 +0530

    css updated for Modal and ProjectModal

--- src/ProjectModal.js
+++ src/ProjectModal.js
@@ -23,28 +23,28 @@ const logos = {'Omdena': Omdena,
 
   return (
     <>
-        <div className="modal-overlay" onClick={onClose} />
-        <div className="modal">
+        <div className="project-modal-overlay" onClick={onClose} />
+        <div className="project-modal">
 
-            <button className="modal-close" onClick={onClose}>&times;</button>
+            <button className="project-modal-close" onClick={onClose}>&times;</button>
             <a href={projData.link}>
-                <div className="modal-img-banner">
+                <div className="project-modal-img-banner">
                     <img src = {logos[projData.logo]}></img>
-                    <div className="modal-project-overlay"></div>
-                    <div className='activityCard__playButton'><BsPlayCircle color='white'/></div>
+                    <div className="project-modal-project-overlay"></div>
+                    <div className='project-activityCard__playButton'><BsPlayCircle color='white'/></div>
                 </div>
             </a>
-            <h1 className="modal-project-name">{projData?.name}</h1>
-            <div className='modal-project__details'>
-                <div className='modal-project__info'>
+            <h1 className="project-modal-project-name">{projData?.name}</h1>
+            <div className='project-modal-project__details'>
+                <div className='project-modal-project__info'>
                     <span><strong>Skills:</strong> {projData?.skills}</span>
                 </div>
-                <div className='modal-project__timeline'>
+                <div className='project-modal-project__timeline'>
                     <div>{projData?.timeline}</div>
                 </div>
             </div>
            
-            <div className='modal-project__description'>
+            <div className='project-modal-project__description'>
                 <h3>Description</h3>
                 <ul>
                     {Object.entries(projData?.description).map(([role, description], index) => (
